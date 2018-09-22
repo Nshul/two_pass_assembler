accb269cd4c1ba3f01ae2c74b0ecb00ddb8f96b4
Anshul Mittal <anshulmittal0409@gmail.com>
2018-09-22 17:05:23 +0530

    Create Hash Map for opcodes
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <cstdio>
+#include <fstream>
+#include <string.h>
 
 using namespace std;
 
@@ -26,20 +27,74 @@ struct Opcode{
     
     // Pointer to next node in LL of Bucket
     Opcode *next;
-};
 
-class HashMapSymbol{
-    int bucketSize;
-    Opcode *Hashmap;
-    public:
-    HashMapSymbol(int s = 13){
-        bucketSize = s;
-        Hashmap = new Opcode[13];
+    Opcode(char opcode_arg[],char machineCode_arg[],char addressFormat_arg[]){
+        strcpy(opcode,opcode_arg);
+        strcpy(machineCode,machineCode_arg);
+        strcpy(addressFormat,addressFormat_arg);
+        next = NULL;
+        cout<<"Creating for "<<opcode<<"*"<<machineCode<<"*"<<addressFormat<<"\n";
     }
 };
 
+class HashMapForOpcode{
+    /*
+        bucketSize -> Holds the current bucket size of HashMap
+        Hashmap -> pointer to the the HashMap with LL
+    */
+    int bucketSize;
+    Opcode **Hashmap;
+
+    /*
+        Calculates Hash Value corresponding to Opcode
+    */
+    int getHashValue(char opcode[]){
+        int value =0;
+        for(int i=0;opcode[i]!='\0';i++){
+            value = (value+(int)opcode[i])%bucketSize;
+        }
+        return value;
+    }
+    
+    public:
+    // Constructor for class with default Bucket Size as 13
+    HashMapForOpcode(int s = 13){
+        bucketSize = s;
+        Hashmap = new Opcode*[13];
+    }
+
+    void insertIntoMap(char opcode[],char machineCode[],char addressFormat[]){
+        Opcode *temp = new Opcode(opcode,machineCode,addressFormat);
+        int hashValue = getHashValue(opcode);
+        if(Hashmap[hashValue]==NULL){
+            Hashmap[hashValue] = temp;
+        }else{
+            Opcode *ptr = Hashmap[hashValue];
+            while(ptr->next!=NULL){
+                ptr = ptr->next;
+            }
+            ptr->next = temp;
+        }
+    }
+
+};
+
 int main(){
-    freopen("Hello.txt","w",stdout);
-    cout<<"U";
+    ifstream opcode_machineCode;
+    
+    /*
+        Here We are making HashMap of Opcodes
+    */
+    opcode_machineCode.open("Opcode_MachineCode.txt");
+    HashMapForOpcode HMap;
+    char opcode[10],machineCode[35],addressFormat[5];
+    while(!opcode_machineCode.eof()){
+        opcode_machineCode>>opcode;
+        opcode_machineCode>>machineCode;
+        opcode_machineCode>>addressFormat;
+        HMap.insertIntoMap(opcode,machineCode,addressFormat);
+    }
+
+    opcode_machineCode.close();
     return 0;
 }
